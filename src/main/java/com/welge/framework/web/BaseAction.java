2738ccd6f926350e2fd1cc21ae5d3291330194a4
haha <haha@hehe>
2015-02-08 17:03:48 +0800

    Signed-off-by: haha <haha@hehe>

---
+++ src/main/java/com/welge/framework/web/BaseAction.java
@@ -0,0 +1,34 @@
+package com.welge.framework.web;
+
+import java.lang.reflect.ParameterizedType;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class BaseAction<T> extends ActionSupport implements ModelDriven<T> {
+	
+	
+	private static final long serialVersionUID = 1L;
+	protected T entity;
+	private Class<T> entityClass;
+	
+
+	@SuppressWarnings("unchecked")
+	public BaseAction(){
+		ParameterizedType parameterizedType = ((ParameterizedType)this.getClass().getGenericSuperclass());
+		entityClass = (Class<T>) parameterizedType.getActualTypeArguments()[0];
+		try {
+			entity = entityClass.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public T getModel() {
+		return entity;
+	}
+
+}
